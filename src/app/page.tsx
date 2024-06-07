13ca83c1a5664123eabb01f2ce8429ebb61c923a
kryparnold <emirhanpgt@gmail.com>
2024-06-08 02:25:22 +0300

    implemented shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,22 @@
-export default function Home() {
+import { auth } from "@/lib/auth";
+
+export default async function Home() {
+    const session = await auth();
+
     return (
-        <main>
-            Hello World!
+        <main className="grid place-items-center h-screen">
+            {session ? (
+                <div>
+
+                </div>
+            ) : (
+                <div>
+                    Lütfen mesaj gönderebilmek veya görmek için giriş yapın.
+                    <div>
+
+                    </div>
+                </div>
+            )}
         </main>
     );
 }
