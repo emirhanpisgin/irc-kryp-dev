bbedc68ec87b0e94160b79fc5c31a5399d311cbb
kryparnold <emirhanpgt@gmail.com>
2024-06-08 02:21:42 +0300

    added authjs with drizzle

---
+++ src/lib/auth.ts
@@ -0,0 +1,11 @@
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
+import NextAuth from "next-auth";
+import { db } from "@/db";
+import github from "next-auth/providers/github";
+import google from "next-auth/providers/google";
+import discord from "next-auth/providers/discord";
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+	adapter: DrizzleAdapter(db),
+	providers: [github, google, discord],
+});
