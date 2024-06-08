67c27ffe7d9194d922078462804cf837094499a4
kryparnold <emirhanpgt@gmail.com>
2024-06-08 19:49:04 +0300

    added some actions

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -3,7 +3,8 @@
 import { auth } from "@/lib/auth";
 import { pusherServer } from "@/lib/pusher";
 import { db } from "@/db";
-import { messages } from "@/db/schema";
+import { messages, users } from "@/db/schema";
+import { eq } from "drizzle-orm";
 
 export async function sendMessageAction(messageContent: string) {
 	const session = await auth();
@@ -18,9 +19,33 @@ export async function sendMessageAction(messageContent: string) {
 		})
 		.returning();
 
-	const res = await pusherServer.trigger("main-channel", "main-event", {
+	await pusherServer.trigger("main-channel", "message-add", {
 		id: message.id,
 		username: session.user?.name,
 		content: messageContent,
 	});
+}
+
+export async function deleteMessageAction(messageId: string) {
+	const session = await auth();
+
+	if (!session) return;
+
+	const [user] = await db.select().from(users).where(eq(users.id, session.user?.id!));
+
+	if (user.id !== process.env.ADMIN_USER_ID) return;
+
+	await db.delete(messages).where(eq(messages.id, messageId));
+
+	await pusherServer.trigger("main-channel", "message-delete", messageId);
+}
+
+export async function changeNameAction(name: string) {
+	const session = await auth();
+
+	if (!session) return;
+
+	await db.update(users).set({
+		name,
+	}).where(eq(users.id, session.user?.id!));
 }
